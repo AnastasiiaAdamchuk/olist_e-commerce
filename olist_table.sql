29c4a0386cc3370d12064a81a98ce40132643b89
AnastasiiaAdamchuk <86518223+AnastasiiaAdamchuk@users.noreply.github.com>
2021-07-04 10:50:58 +0200

    Update olist_table.sql

--- olist_table.sql
+++ olist_table.sql
@@ -3,3 +3,6 @@
 -- SELECT * FROM olist.olist_order_items_dataset LIMIT 10	
 -- SELECT * FROM olist.olist_marketing_qualified_leads_dataset LIMIT 10	
 -- SELECT * FROM olist.olist_closed_deals_dataset LIMIT 10	
+
+-- measures and dimentions:
+-- olist.olist_products_dataset pd, 
