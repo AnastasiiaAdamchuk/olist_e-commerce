1f5cac6279948daa6d8ccdc3d189f02bee3707f9
AnastasiiaAdamchuk <86518223+AnastasiiaAdamchuk@users.noreply.github.com>
2021-07-04 10:47:56 +0200

    Create olist_table.sql

---
+++ olist_table.sql
@@ -0,0 +1,5 @@
+-- tables:
+-- SELECT * FROM olist.olist_products_dataset LIMIT 10	
+-- SELECT * FROM olist.olist_order_items_dataset LIMIT 10	
+-- SELECT * FROM olist.olist_marketing_qualified_leads_dataset LIMIT 10	
+-- SELECT * FROM olist.olist_closed_deals_dataset LIMIT 10	
