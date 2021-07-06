203f16fc8562aa635b82d7c477bdd533cba1b3c8
AnastasiiaAdamchuk <86518223+AnastasiiaAdamchuk@users.noreply.github.com>
2021-07-06 16:06:22 +0200

    Update olist_table.sql

--- olist_table.sql
+++ olist_table.sql
@@ -5,10 +5,10 @@
 -- SELECT * FROM olist.olist_closed_deals_dataset LIMIT 10	
 
 -- measures and dimentions:
-
+-- 
 -- mqld olist.olist_marketing_qualified_leads_dataset --> mqld.mql_id, mqld.first_contact_date, mqld.origin
 -- cdd olist.olist_closed_deals_dataset --> cdd.mql_id, cdd.seller_id, cdd.business_segment, cdd.sdr_id, cdd.sr_id, cdd.won_date
--- oi olist.olist_order_items_dataset --> oi.order_id, oi.product_id, oi.seller_id, oi.price, oi.freight_value
+-- oid olist.olist_order_items_dataset --> oid.order_id, oid.order_item_id, oid.product_id, oid.seller_id, oid.price, oid.freight_value
 -- pd olist.olist_products_dataset --> pd.product_id, pd.product_category_name, pd.product_weight_g
 
 
